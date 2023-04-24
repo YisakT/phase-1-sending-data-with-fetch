377f36910809407bca95b1bd14730f8c0750685e
Yisak Girmay <yisakbruck23@gmail.com>
2023-04-24 14:48:40 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Add your code here
+
+
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  };
+  
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  
+  fetch("http://localhost:3000/dogs", configurationObject)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object){
+    console.log(object);
+  })
+  .catch(function (error) {
+    alert("Caution amature coder");
+    console.log(error.message);
+  })
+  
+  function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        name,
+        email
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+      document.body.innerHTML = result.id
+    })
+    .catch(error => {
+      document.body.innerHTML = error.message
+    })
+  }
+  
